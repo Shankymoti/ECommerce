e432c6a8439349a4cc6f1278105df695ba8e8c83
Shankymoti <shashankkumar648@gmail.com>
2024-09-13 02:14:33 +0530

    secondCommit

--- src/test/java/pageObjects/CartPage.java
+++ src/test/java/pageObjects/CartPage.java
@@ -1,5 +1,18 @@
 package pageObjects;
 
-public class CartPage {
+import java.util.List;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class CartPage extends BasePage{
+
+	//constructor
+	public CartPage(WebDriver driver) {
+		super(driver);
+	}
+	
+	@FindBy(css=".cartSection h3")
+	List<WebElement>  cartProducts;
 }
